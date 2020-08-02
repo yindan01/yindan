a69dca472c9ff2db0370fd160c7366455ed57536
yindan01 <1130083087@qq.com>
2020-08-02 00:21:04 -0500

    ##学习记录 ###今天交了第一次作业

---
+++ tonglao.py
@@ -0,0 +1,13 @@
+##定义一个童姥的类
+class TongLao:
+#属性
+      TongLao_hp=1000
+      TongLao_Force=1500
+      WYZ_hp=1000
+      WYZ_Force=2000
+      Tianshanzhemeishou_hp=1000
+      Tianshanzhemeishou_Force=1200
+      XuZhu_hp=1500
+      XuZhu_Force=1300
+ TongLao_hp=WYZ_Force-
+
